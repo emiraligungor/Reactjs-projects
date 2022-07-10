acd39d497e74fe8eb526069a76f1df44fe6a61bd
Emirali Gungor <92479911+emiraligungor@users.noreply.github.com>
2022-07-10 17:44:09 +0200

    Update config.js

--- 6.finance-tracker-project/src/firebase/config.js
+++ 6.finance-tracker-project/src/firebase/config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD7yujV-67ZBKrkac0ip8Rwv9-SgAXIaPg",
-    authDomain: "finance-tracker-project-4b2b8.firebaseapp.com",
-    projectId: "finance-tracker-project-4b2b8",
-    storageBucket: "finance-tracker-project-4b2b8.appspot.com",
-    messagingSenderId: "271873726039",
-    appId: "1:271873726039:web:a03b0dc8d75e977585b512"
+    apiKey: "API KEY",
+    authDomain: "authDomain",
+    projectId: "pID",
+    storageBucket: "stgBucke",
+    messagingSenderId: "mID",
+    appId: "appId"
   };
 
 //init firebase
